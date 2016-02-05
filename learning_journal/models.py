27acbba9a2eec52f8b52375ff55610649db8891f
Erica Winberry <elaewin@gmail.com>
2016-02-04 18:16:10 -0800

    Fixed syntax errors in line 45 (the list comprehension).

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -30,12 +30,20 @@ class MyModel(Base):
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
 
+
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
     title = Column(Text, unique=True, mysql_length=255, CheckConstraint('title!=""'))
     body = Column(Text, default='')
     created = Column(DateTime, default=datetime.datetime.utcnow)
-    edited = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, onupdate=datetime.datetime.utcnow)
 
     @classmethod
+    def all(cls):
+        all_entries = session.query(Entry).order_by(desc(Entry.id))
+        return [(entry.title, entry.body) for entry in all_entries]
+
+    @classmethod
+    def by_id(cls, requested_id):
+        return session.query(Entry).get(requested_id)
