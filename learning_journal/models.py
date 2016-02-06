7d594db90ccf26a550c2c77fd75f595a92372777
Erica Winberry <elaewin@gmail.com>
2016-02-05 17:55:18 -0800

    Changed main in initializedb.py so that the correct fields for Entry are included. Removed MyModel from models.py.

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -23,11 +23,11 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 query = DBSession.query_property()
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+# class MyModel(Base):
+#     __tablename__ = 'models'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(Text)
+#     value = Column(Integer)
 
 # Index('my_index', MyModel.name, unique=True, mysql_length=255)
 
