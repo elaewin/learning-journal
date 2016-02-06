9a04ba649c7c6e2d422e0a01c857bcbd7989ce4c
Erica Winberry <elaewin@gmail.com>
2016-02-06 14:31:53 -0800

    Changed class methods to include a session parameter that defaults to DBSession, changed CheckConstraint in title column to nullable=False.

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -22,24 +22,27 @@ from zope.sqlalchemy import ZopeTransactionExtension
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
-query = DBSession.query_property()
 
 
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
-    title = Column(Unicode(255), CheckConstraint('title!=""'), unique=True)
+    title = Column(Unicode(length=255), nullable=False, unique=True)
     body = Column(Unicode, default='')
     created = Column(DateTime, default=datetime.utcnow)
     edited = Column(DateTime, onupdate=datetime.utcnow)
 
     @classmethod
-    def all(cls):
-        all_entries = cls.query(Entry).order_by(desc(Entry.id))
-        return [(entry.title) for entry in all_entries]
+    def all(cls, session=None):
+        if session is None:
+            session = DBSession
+        all_entries = session.query(cls).order_by(desc(cls.id))
+        return all_entries
 
     @classmethod
-    def by_id(cls, requested_id):
-        return cls.query(Entry).get(requested_id)
+    def by_id(cls, requested_id, session=None):
+        if session is None:
+            session = DBSession
+        return session.query(cls).get(requested_id)
 
 # Index('my_index', Entry.title, unique=True, mysql_length=255)
