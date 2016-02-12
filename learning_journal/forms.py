b38966be3e1828d20fd281dc7e02c547bca08ce8
Erica Winberry <elaewin@gmail.com>
2016-02-12 13:40:30 -0800

    Created EntryEditForm class.

--- learning_journal/forms.py
+++ learning_journal/forms.py
@@ -2,6 +2,7 @@ from wtforms import Form, TextField, TextAreaField, validators
 
 strip_filter = lambda x: x.strip() if x else None
 
+
 class EntryCreateForm(Form):
     title = TextField(
         'Entry title',
@@ -11,3 +12,18 @@ class EntryCreateForm(Form):
         'Entry body',
         [validators.Length(min=1)],
         filters=[strip_filter])
+
+
+class EntryEditForm(Form):
+    title = TextField(
+        'Entry title',
+        [validators.Length(min=1, max=255)],
+        filters=[strip_filter],
+        default= # .get the existing name for the title here)
+    '''from WTForms docs: If you just need to access the data for known
+    fields, you should use form.<field>.data, not this proxy property.'''
+    body = TextAreaField(
+        'Entry body',
+        [validators.Length(min=1)],
+        filters=[strip_filter],
+        default= # .get the contents of the body field here)
