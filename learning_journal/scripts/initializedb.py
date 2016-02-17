ed3ea7e3fa13cd65e6065c1e336b27b3480ab131
Erica Winberry <elaewin@gmail.com>
2016-02-16 20:59:55 -0800

    fixed initializedb.py

--- learning_journal/scripts/initializedb.py
+++ learning_journal/scripts/initializedb.py
@@ -37,12 +37,12 @@ def main(argv=sys.argv):
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
+    if 'DATABASE_URL' in os.environ:
+        settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
     with transaction.manager:
-        # model = Entry(title='Entry Title', body='', created=datetime.utcnow(), edited=datetime.utcnow())
-        # DBSession.add(model)
         encrypted = password_context.encrypt('admin')
         admin = User(username='admin', password=encrypted)
         DBSession.add(admin)
